d3fe83edcbbcbef5198482754e9c8c85c6d0fd49
Justin Ahinon <justiny.ahinon@gmail.com>
2022-05-06 10:33:50 +0200

    Move the generated files to the `.arifacts` folder

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,14 +4,14 @@ import scrape from 'website-scraper';
 const saveOutputToJson = (output) => {
     const date = new Date();
     const fileName = `blocks-${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.json`;
-    const filePath = `./blocks/${fileName}`;
+    const filePath = `./.artifacts/blocks/${fileName}`;
 
     fs.writeFileSync(filePath, JSON.stringify(output, null, 2));
 }
 
 const downloadGeneratedPage = async (url) => {
     const date = new Date();
-    const path = `./generated-pages/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
+    const path = `./.artifacts/generated-pages/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`;
 
     const options = {
         urls: [url],
