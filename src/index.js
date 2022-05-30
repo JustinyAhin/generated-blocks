a3c1093368144dd26d21a10decac840e94e255f4
Justin Ahinon <justiny.ahinon@gmail.com>
2022-05-30 12:14:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,29 +12,105 @@ import { login, publishPage, retrieveAllBlocks } from "./utils/browser.js";
     // Login
     const page = await login(browser);
 
+
+    const insertBlock = async (blockRepresentation) => {
+        await page.evaluate((_blockRepresentation) => {
+            function recursiveCreateBlock({ name, attributes = {}, innerBlocks = [], }) {
+                return window.wp.blocks.createBlock(name, attributes, innerBlocks.map((innerBlock) => recursiveCreateBlock(innerBlock)));
+            }
+            const block = recursiveCreateBlock(_blockRepresentation);
+            window.wp.data.dispatch('core/block-editor').insertBlock(block);
+        }, blockRepresentation);
+    }
+
     // Create a new page
     await page.goto(`${baseUrl}/wp-admin/post-new.php?post_type=page&post_title=Generated+Blocks`);
 
     // Retrieve all Core blocks
     const allBlocks = await retrieveAllBlocks(page);
 
-    const blocksWithContentAttribute = allBlocks.filter(block => block.attributes.content);
-    await page.evaluate(blocksWithContentAttribute => {
-        blocksWithContentAttribute.forEach(block => {
-            const createBlock = window.wp.blocks.createBlock(block.name, {
-                content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, reiciendis?",
-            });
-            window.wp.data.dispatch('core/block-editor').insertBlock(createBlock);
-        });
-    }, blocksWithContentAttribute);
+    const blocksByCategory = allBlocks.reduce((acc, block) => {
+        if (block.category) {
+            if (!acc[block.category]) {
+                acc[block.category] = [];
+            }
+            acc[block.category].push(block.name);
+        }
+        return acc;
+    }, {});
 
-    const blocksWithoutContentAttribute = allBlocks.filter(block => !block.attributes.content);
-    await page.evaluate(blocksWithoutContentAttribute => {
-        blocksWithoutContentAttribute.forEach(block => {
-            const createBlock = window.wp.blocks.createBlock(block.name);
-            window.wp.data.dispatch('core/block-editor').insertBlock(createBlock);
+    // Text blocks
+    const textBlocks = ["core/paragraph", "core/heading", "core/freeform", "core/code", "core/missing", "core/preformatted", "core/verse"]
+
+    const content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, reiciendis?";
+
+    textBlocks.forEach(async (blockName) => {
+        await insertBlock({
+            name: blockName,
+            attributes: {
+                content,
+            }
         });
-    }, blocksWithoutContentAttribute);
+    });
+
+    // Quote blocks
+    const quoteBlocks = ["core/quote", "core/pullquote"];
+
+    const citation = "Jane Doe";
+    const value = "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, reiciendis?</p>";
+
+    quoteBlocks.forEach(async (blockName) => {
+        await insertBlock({
+            name: blockName,
+            attributes: {
+                value,
+                citation,
+            }
+        });
+    });
+
+
+    // List block
+    const values = `<li>First list item</li>
+        <li>Second list item</li>
+        <li>Third list item</li>
+        <li>Fourth list item</li>
+        <li>Fifth list item</li>`;
+
+    await insertBlock({
+        name: "core/list",
+        attributes: {
+            values,
+        }
+    });
+
+    // Image block
+    const imageData = {
+        url: "https://picsum.photos/1200/450",
+        alt: "Picsum image 1200x450",
+    }
+
+    await insertBlock({
+        name: "core/image",
+        attributes: {
+            url: imageData.url,
+            alt: imageData.alt,
+        }
+    });
+
+
+    // Cover block
+    await insertBlock({
+        name: "core/cover",
+        attributes: {
+            url: imageData.url,
+            alt: imageData.alt,
+            dimRatio: 30,
+        }
+    });
+
+
+    await page.waitForTimeout(20000);
 
     // Publish the page
     await publishPage(page);
