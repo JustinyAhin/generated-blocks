af7762c58aaa53fc407d882e17b72bbfd28b0e6c
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-14 16:18:29 +0200

    Update misc.ts

--- src/misc.ts
+++ src/misc.ts
@@ -4,13 +4,13 @@ import { DateTime } from "luxon";
 
 function downloadGeneratedPage(url: string, dateString: string): void {
   execSync(
-    `node-site-downloader download -d https://segbedji.com/a-propos -s https://segbedji.com/a-propos -o ./artifacts/${dateString}/text --include-images`
+    `node-site-downloader download -d https://segbedji.com/a-propos -s https://segbedji.com/a-propos -o ./artifacts/${dateString}/text --outputFolderSuffix blocks --include-images`
   );
 }
 
 function getCurrentDateTime(): string {
   let currentDateTime = DateTime.utc()
-    .toLocaleString(DateTime.DATETIME_FULL)
+    .toLocaleString(DateTime.DATE_MED)
     .toLocaleLowerCase();
   return currentDateTime.replace(/\s/g, "-").replace(/,/g, "");
 }
