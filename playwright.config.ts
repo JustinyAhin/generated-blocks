6270c31bb776d3fcdce0ebce407bf6cf8ec244d2
Justin Ahinon <justiny.ahinon@gmail.com>
2022-06-14 16:42:20 +0200

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ const config: PlaywrightTestConfig = {
   expect: {
     timeout: 10000,
   },
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
